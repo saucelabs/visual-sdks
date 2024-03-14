d8792f6ae638f17a5421feaa4842fe49c9d82075
Félix P <felix@saucelabs.com>
2024-03-14 15:48:20 +0100

    Simplify VisualClient Interface (#2)

--- visual-dotnet/SauceLabs.Visual/VisualClient.cs
+++ visual-dotnet/SauceLabs.Visual/VisualClient.cs
@@ -14,13 +14,15 @@ namespace SauceLabs.Visual
     /// <summary>
     /// <c>VisualClient</c> provides an access to Sauce Labs Visual services.
     /// </summary>
-    public class VisualClient
+    public class VisualClient : IDisposable
     {
         private readonly VisualApi<WebDriver> _api;
         private readonly string _sessionId;
         private readonly string _jobId;
         private readonly string _sessionMetadataBlob;
         private readonly List<string> _screenshotIds = new List<string>();
+        public VisualBuild Build { get; }
+        private readonly bool _externalBuild;
         public bool CaptureDom { get; set; } = false;
 
         /// <summary>
@@ -30,7 +32,19 @@ namespace SauceLabs.Visual
         /// <param name="region">the Sauce Labs region to connect to</param>
         /// <param name="username">the Sauce Labs username</param>
         /// <param name="accessKey">the Sauce Labs access key</param>
-        public VisualClient(WebDriver wd, Region region, string username, string accessKey)
+        public VisualClient(WebDriver wd, Region region, string username, string accessKey) : this(wd, region, username, accessKey, new CreateBuildOptions())
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of <c>VisualClient</c>
+        /// </summary>
+        /// <param name="wd">the instance of the WebDriver session</param>
+        /// <param name="region">the Sauce Labs region to connect to</param>
+        /// <param name="username">the Sauce Labs username</param>
+        /// <param name="accessKey">the Sauce Labs access key</param>
+        /// <param name="buildOptions">the options of the build creation</param>
+        public VisualClient(WebDriver wd, Region region, string username, string accessKey, CreateBuildOptions buildOptions)
         {
             _api = new VisualApi<WebDriver>(wd, region, username, accessKey);
             _sessionId = wd.SessionId.ToString();
@@ -38,6 +52,10 @@ namespace SauceLabs.Visual
             var response = _api.WebDriverSessionInfo(_jobId, _sessionId).Result;
             var metadata = response.EnsureValidResponse();
             _sessionMetadataBlob = metadata.Result.Blob;
+
+            var createBuildResponse = CreateBuild(buildOptions).Result;
+            Build = new VisualBuild(createBuildResponse.Id, createBuildResponse.Url);
+            _externalBuild = false;
         }
 
         /// <summary>
@@ -45,7 +63,7 @@ namespace SauceLabs.Visual
         /// </summary>
         /// <param name="options">the options for the build creation</param>
         /// <returns>a <c>VisualBuild</c> instance</returns>
-        public async Task<VisualBuild> CreateBuild(CreateBuildOptions? options = null)
+        private async Task<VisualBuild> CreateBuild(CreateBuildOptions? options = null)
         {
             var result = (await _api.CreateBuild(new CreateBuildIn
             {
@@ -61,7 +79,7 @@ namespace SauceLabs.Visual
         /// <c>FinishBuild</c> finishes a build
         /// </summary>
         /// <param name="build">the build to finish</param>
-        public async Task FinishBuild(VisualBuild build)
+        private async Task FinishBuild(VisualBuild build)
         {
             (await _api.FinishBuild(build.Id)).EnsureValidResponse();
         }
@@ -73,14 +91,14 @@ namespace SauceLabs.Visual
         /// <param name="name">the name of the screenshot</param>
         /// <param name="options">the configuration for the screenshot capture and comparison</param>
         /// <returns></returns>
-        public async Task<string> VisualCheck(VisualBuild build, string name, VisualCheckOptions? options = null)
+        public async Task<string> VisualCheck(string name, VisualCheckOptions? options = null)
         {
             var ignored = new List<RegionIn>();
             ignored.AddRange(options?.IgnoreRegions?.Select(r => new RegionIn(r)) ?? new List<RegionIn>());
             ignored.AddRange(options?.IgnoreElements?.Select(r => new RegionIn(r)) ?? new List<RegionIn>());
 
             var result = (await _api.CreateSnapshotFromWebDriver(new CreateSnapshotFromWebDriverIn(
-                buildUuid: build.Id,
+                buildUuid: Build.Id,
                 name: name,
                 jobId: _jobId,
                 diffingMethod: options?.DiffingMethod ?? DiffingMethod.Simple,
@@ -93,12 +111,27 @@ namespace SauceLabs.Visual
         }
 
         /// <summary>
+        /// <c>Cleanup</c> set a correct status to the build. No action should be made after that calling <c>Cleanup</c>.
+        /// </summary>
+        public async Task Cleanup()
+        {
+            if (!_externalBuild)
+            {
+                await FinishBuild(Build);
+            }
+        }
+
+        public void Dispose()
+        {
+            _api.Dispose();
+        }
+
+        /// <summary>
         /// <c>VisualResults</c> returns the results of screenshot comparison.
         /// </summary>
-        /// <param name="buildId">the id of the build to check.</param>
         /// <returns>a dictionary containing <c>DiffStatus</c> and the number of screenshot in that status.</returns>
         /// <exception cref="VisualClientException"></exception>
-        public async Task<Dictionary<DiffStatus, int>> VisualResults(string buildId)
+        public async Task<Dictionary<DiffStatus, int>> VisualResults()
         {
             var policyOptions = new RetryPolicyOptions
             {
@@ -106,7 +139,7 @@ namespace SauceLabs.Visual
                 MaxRetryInterval = TimeSpan.FromSeconds(5),
                 MaxRetrySteps = 10
             };
-            var result = await Retry.Do(async () => await FetchVisualResults(buildId),
+            var result = await Retry.Do(async () => await FetchVisualResults(Build.Id),
                 retryInterval: TimeSpan.FromMilliseconds(100),
                 retryLimit: 10,
                 retryPolicy: RetryPolicy.ExponentialBackoff,
