2ce3c72b60dd7bf996dc767509488be9a2753f0a
Felix P <felix@saucelabs.com>
2024-03-19 10:14:02 +0100

    Not nullable envs

--- visual-dotnet/SauceLabs.Visual/Utils/EnvVars.cs
+++ visual-dotnet/SauceLabs.Visual/Utils/EnvVars.cs
@@ -4,9 +4,9 @@ namespace SauceLabs.Visual.Utils
 {
     internal class EnvVars
     {
-        internal static string? CustomId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_CUSTOM_ID");
-        internal static string? BuildId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BUILD_ID");
-        internal static string? Username => Environment.GetEnvironmentVariable("SAUCE_USERNAME");
-        internal static string? AccessKey => Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY");
+        internal static string CustomId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_CUSTOM_ID") ?? "";
+        internal static string BuildId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BUILD_ID") ?? "";
+        internal static string Username => Environment.GetEnvironmentVariable("SAUCE_USERNAME") ?? "";
+        internal static string AccessKey => Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY") ?? "";
     }
 }
--- visual-dotnet/SauceLabs.Visual/VisualClient.cs
+++ visual-dotnet/SauceLabs.Visual/VisualClient.cs
@@ -52,7 +52,7 @@ namespace SauceLabs.Visual
         /// <param name="region">the Sauce Labs region to connect to</param>
         /// <param name="username">the Sauce Labs username</param>
         /// <param name="accessKey">the Sauce Labs access key</param>
-        public VisualClient(WebDriver wd, Region region, string? username, string? accessKey) : this(wd, region, username, accessKey, new CreateBuildOptions())
+        public VisualClient(WebDriver wd, Region region, string username, string accessKey) : this(wd, region, username, accessKey, new CreateBuildOptions())
         {
         }
 
@@ -64,14 +64,14 @@ namespace SauceLabs.Visual
         /// <param name="username">the Sauce Labs username</param>
         /// <param name="accessKey">the Sauce Labs access key</param>
         /// <param name="buildOptions">the options of the build creation</param>
-        public VisualClient(WebDriver wd, Region region, string? username, string? accessKey, CreateBuildOptions buildOptions)
+        public VisualClient(WebDriver wd, Region region, string username, string accessKey, CreateBuildOptions buildOptions)
         {
             if (string.IsNullOrEmpty(username?.Trim()) || string.IsNullOrEmpty(accessKey?.Trim()))
             {
                 throw new VisualClientException("Username or Access Key not set");
             }
 
-            _api ??= new VisualApi<WebDriver>(wd, region, username!, accessKey!);
+            _api = new VisualApi<WebDriver>(wd, region, username!, accessKey!);
             _sessionId = wd.SessionId.ToString();
             _jobId = wd.Capabilities.HasCapability("jobUuid") ? wd.Capabilities.GetCapability("jobUuid").ToString() : _sessionId;
             var response = _api.WebDriverSessionInfo(_jobId, _sessionId).Result;
