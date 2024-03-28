19524f210d9ce7c751f0a9344d2726b074d25463
Felix P <felix@saucelabs.com>
2024-03-28 16:29:03 +0100

    Cleanup

--- visual-dotnet/SauceLabs.Visual.Tests/BuildFactoryTest.cs
+++ visual-dotnet/SauceLabs.Visual.Tests/BuildFactoryTest.cs
@@ -1,18 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using GraphQL.Client.Serializer.Newtonsoft;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using RichardSzalay.MockHttp;
 using SauceLabs.Visual.GraphQL;
 using SauceLabs.Visual.Models;
-using SauceLabs.Visual.Tests.Utils;
 
 namespace SauceLabs.Visual.Tests;
 
