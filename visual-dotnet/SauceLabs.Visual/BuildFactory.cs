ce4201cd716ea56546837c57129ae978cc62a372
Félix P <felix@saucelabs.com>
2024-04-23 14:07:30 +0200

    bugfix: .Net / NullReferenceException when buildId or customId is not found (#34)

--- visual-dotnet/SauceLabs.Visual/BuildFactory.cs
+++ visual-dotnet/SauceLabs.Visual/BuildFactory.cs
@@ -93,6 +93,11 @@ namespace SauceLabs.Visual
             try
             {
                 var build = (await api.Build(buildId)).EnsureValidResponse().Result;
+                if (build == null)
+                {
+                    throw new VisualClientException($@"build {buildId} was not found");
+                }
+
                 return new VisualBuild(build.Id, build.Url, build.Mode);
             }
             catch (VisualClientException)
@@ -113,6 +118,11 @@ namespace SauceLabs.Visual
             try
             {
                 var build = (await api.BuildByCustomId(customId)).EnsureValidResponse().Result;
+                if (build == null)
+                {
+                    throw new VisualClientException($@"build identified by {customId} was not found");
+                }
+
                 return new VisualBuild(build.Id, build.Url, build.Mode);
             }
             catch (VisualClientException)
