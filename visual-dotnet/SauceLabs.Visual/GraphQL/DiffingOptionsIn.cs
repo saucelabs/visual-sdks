d3572d6be64dc553e8c67d238643b844d4df93fe
Felix P <felix@saucelabs.com>
2024-05-02 09:48:26 +0200

    Styling

--- visual-dotnet/SauceLabs.Visual/GraphQL/DiffingOptionsIn.cs
+++ visual-dotnet/SauceLabs.Visual/GraphQL/DiffingOptionsIn.cs
@@ -4,12 +4,18 @@ namespace SauceLabs.Visual.GraphQL
 {
     public class DiffingOptionsIn
     {
-        [JsonProperty("content")] public bool Content { get; set; }
-        [JsonProperty("dimensions")] public bool Dimensions { get; set; }
-        [JsonProperty("position")] public bool Position { get; set; }
-        [JsonProperty("structure")] public bool Structure { get; set; }
-        [JsonProperty("style")] public bool Style { get; set; }
-        [JsonProperty("visual")] public bool Visual { get; set; }
+        [JsonProperty("content")]
+        public bool Content { get; set; }
+        [JsonProperty("dimensions")]
+        public bool Dimensions { get; set; }
+        [JsonProperty("position")]
+        public bool Position { get; set; }
+        [JsonProperty("structure")]
+        public bool Structure { get; set; }
+        [JsonProperty("style")]
+        public bool Style { get; set; }
+        [JsonProperty("visual")]
+        public bool Visual { get; set; }
 
         public DiffingOptionsIn(bool defaultValue)
         {
