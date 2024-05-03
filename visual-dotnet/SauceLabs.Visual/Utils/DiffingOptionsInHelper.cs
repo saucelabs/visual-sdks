04577169d256519d6817d65576f4253eed7ee381
Felix P <felix@saucelabs.com>
2024-05-03 11:24:04 +0200

    cleanup

--- visual-dotnet/SauceLabs.Visual/Utils/DiffingOptionsInHelper.cs
+++ visual-dotnet/SauceLabs.Visual/Utils/DiffingOptionsInHelper.cs
@@ -7,34 +7,6 @@ namespace SauceLabs.Visual.Utils
 {
     public static class DiffingOptionsInHelper
     {
-        private static readonly Dictionary<DiffingOption, Func<DiffingOptionsIn, bool, DiffingOptionsIn>> Mapping = new Dictionary<DiffingOption, Func<DiffingOptionsIn, bool, DiffingOptionsIn>>()
-        {
-            { DiffingOption.Content, (diffingOptions, b) => {
-                diffingOptions.Content = b;
-                return diffingOptions;
-            } },
-            { DiffingOption.Dimensions, (diffingOptions, b) => {
-                diffingOptions.Dimensions = b;
-                return diffingOptions;
-            } },
-            { DiffingOption.Position, (diffingOptions, b) => {
-                diffingOptions.Position = b;
-                return diffingOptions;
-            } },
-            { DiffingOption.Structure, (diffingOptions, b) => {
-                diffingOptions.Structure = b;
-                return diffingOptions;
-            } },
-            { DiffingOption.Style, (diffingOptions, b) => {
-                diffingOptions.Style = b;
-                return diffingOptions;
-            } },
-            { DiffingOption.Visual, (diffingOptions, b) => {
-                diffingOptions.Visual = b;
-                return diffingOptions;
-            } },
-        };
-
         private static DiffingOptionsIn SetOptions(DiffingOptionsIn opts, DiffingOption flags, bool value)
         {
             if (flags.HasFlag(DiffingOption.Content))
