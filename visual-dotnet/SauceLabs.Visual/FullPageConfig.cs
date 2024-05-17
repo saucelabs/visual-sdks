8e7ec88fe2031586a9572d1ad83da77fcfceff88
Pawel Tomaszewski <pawel.tomaszewski@saucelabs.com>
2024-05-17 13:56:49 +0200

    IRIS-928-fps-extend-options

--- visual-dotnet/SauceLabs.Visual/FullPageConfig.cs
+++ visual-dotnet/SauceLabs.Visual/FullPageConfig.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using SauceLabs.Visual.GraphQL;
 
@@ -6,14 +7,35 @@ namespace SauceLabs.Visual
     public class FullPageConfig
     {
         public int? DelayAfterScrollMs { get; set; }
+        public bool? DisableCSSAnimation { get; set; }
         public IEnumerable<string>? HideAfterFirstScroll { get; set; }
+        public bool? HideScrollBars { get; set; }
+        private int? _scrollLimit;
+        public int? ScrollLimit
+        {
+            get { return _scrollLimit; }
+            set
+            {
+                if (value > 0 && value <= 10)
+                {
+                    _scrollLimit = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(ScrollLimit), "Value must be between 1 and 10.");
+                }
+            }
+        }
 
         internal FullPageConfigIn ToFullPageConfigIn()
         {
             return new FullPageConfigIn()
             {
                 DelayAfterScrollMs = DelayAfterScrollMs,
-                HideAfterFirstScroll = HideAfterFirstScroll
+                DisableCSSAnimation = DisableCSSAnimation,
+                HideAfterFirstScroll = HideAfterFirstScroll,
+                HideScrollBars = HideScrollBars,
+                ScrollLimit = ScrollLimit
             };
         }
     }
