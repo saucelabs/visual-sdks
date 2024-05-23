3c46a7db600dc14899687d934584f2e91dd1a49c
Logan Graham <logan.graham@saucelabs.com>
2024-05-23 17:41:05 -0400

    fix prettier lint

--- visual-js/visual-wdio/src/guarded-types.ts
+++ visual-js/visual-wdio/src/guarded-types.ts
@@ -4,21 +4,21 @@ import { makeValidate, RegionIn } from '@saucelabs/visual';
 export type WdioElement = WebdriverIO.Element;
 
 const wdioElementType = type({
-    elementId: 'string',
-    selector: 'string',
+  elementId: 'string',
+  selector: 'string',
 });
 
 export const isWdioElement = wdioElementType.allows as (
-    x: unknown,
+  x: unknown,
 ) => x is WdioElement;
 
 export const validateWdioElement = makeValidate(wdioElementType) as (
-    x: unknown,
+  x: unknown,
 ) => WdioElement;
 
 export type Ignorable =
-    | WdioElement
-    | WdioElement[]
-    | Promise<WdioElement>
-    | Promise<WdioElement[]>
-    | RegionIn;
+  | WdioElement
+  | WdioElement[]
+  | Promise<WdioElement>
+  | Promise<WdioElement[]>
+  | RegionIn;
