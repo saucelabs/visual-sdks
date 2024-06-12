f1a00afadf084b8be4d0bc6ca27e80bcc41dc616
Kerem <github@beygo.info>
2024-06-12 12:23:21 +0200

    wdio env var IT

---
+++ visual-js/visual-wdio/integration-tests/pages/page.ts
@@ -0,0 +1,42 @@
+/**
+ * main page object containing all methods, selectors and functionality
+ * that is shared across all page objects
+ */
+export default class Page {
+  private selector;
+
+  constructor(selector) {
+    this.selector = selector;
+  }
+
+  /**
+   * Opens a sub page of the page
+   * @param path path of the sub page (e.g. /path/to/page.html)
+   */
+  public open(path?: string) {
+    return browser.url(`https://saucedemo.com${path}`);
+  }
+
+  /**
+   * Wait for the element to be displayed
+   */
+  async waitForIsShown(isShown = true): Promise<boolean> {
+    try {
+      const result = await $(this.selector).waitForDisplayed({
+        timeout: 30 * 1000,
+        reverse: !isShown,
+      });
+
+      return !!result;
+    } catch (e) {
+      return !isShown;
+    }
+  }
+
+  /**
+   * Give back if the element is displayed
+   */
+  async isDisplayed(): Promise<boolean> {
+    return $(this.selector).isDisplayed();
+  }
+}
