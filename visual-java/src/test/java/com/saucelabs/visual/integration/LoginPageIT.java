f2cb2e494d41b1f79534047511b47b9792d280cb
Kerem <github@beygo.info>
2024-08-02 13:20:10 +0300

    Add clipElement to IT

--- visual-java/src/test/java/com/saucelabs/visual/integration/LoginPageIT.java
+++ visual-java/src/test/java/com/saucelabs/visual/integration/LoginPageIT.java
@@ -4,10 +4,12 @@ import com.saucelabs.saucebindings.SaucePlatform;
 import com.saucelabs.saucebindings.UnhandledPromptBehavior;
 import com.saucelabs.saucebindings.junit5.SauceBaseTest;
 import com.saucelabs.saucebindings.options.SauceOptions;
+import com.saucelabs.visual.CheckOptions;
 import com.saucelabs.visual.VisualApi;
 import com.saucelabs.visual.junit5.TestMetaInfoExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
 
 @ExtendWith({TestMetaInfoExtension.class})
 class LoginPageIT extends SauceBaseTest {
@@ -25,7 +27,12 @@ class LoginPageIT extends SauceBaseTest {
     VisualApi visual =
         new VisualApi(driver, System.getenv("SAUCE_USERNAME"), System.getenv("SAUCE_ACCESS_KEY"));
     driver.get("https://www.saucedemo.com");
-    visual.sauceVisualCheck("Login page");
+    visual.sauceVisualCheck(
+        "Login page",
+        new CheckOptions.Builder()
+            .withClipElement(
+                driver.findElement(By.cssSelector("input[data-test=\"login-button\"]")))
+            .build());
     System.out.println("Sauce Visual: " + visual.getBuild().getUrl());
   }
 }
