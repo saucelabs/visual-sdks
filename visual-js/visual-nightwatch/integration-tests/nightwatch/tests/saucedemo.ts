40054c5934e0ab62a0d1d65b015d544834d0049e
Kerem <github@beygo.info>
2024-08-05 13:22:52 +0200

    separate clip test

--- visual-js/visual-nightwatch/integration-tests/nightwatch/tests/saucedemo.ts
+++ visual-js/visual-nightwatch/integration-tests/nightwatch/tests/saucedemo.ts
@@ -2,6 +2,9 @@ import { NightwatchTests } from 'nightwatch';
 
 const home: NightwatchTests = {
   'Check Login Page': () => {
+    browser.url('https://saucedemo.com').sauceVisualCheck('Login page');
+  },
+  'Check Login Page - clipped': () => {
     browser.url('https://saucedemo.com').sauceVisualCheck('Login page', {
       clipElement: browser.element('input[data-test="login-button"]'),
     });
