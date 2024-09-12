93b95eaae8777cceca6d1ee7e8423633313d590a
Logan Graham <logan.graham@saucelabs.com>
2024-09-12 17:16:27 -0400

    add playwright integration tests

---
+++ visual-js/visual-playwright/integration-tests/custom-test.ts
@@ -0,0 +1,12 @@
+import { test as base } from "@playwright/test";
+import { sauceVisualFixtures, SauceVisualFixtures } from "@saucelabs/visual-playwright";
+
+export const test = base.extend<SauceVisualFixtures>({
+    // Set up the Sauce Visual fixture, and optionally customize the global options which are sent
+    // with each sauce visual check to reduce duplication.
+    ...sauceVisualFixtures({
+        captureDom: true,
+    }),
+});
+
+export { expect } from "@playwright/test";
