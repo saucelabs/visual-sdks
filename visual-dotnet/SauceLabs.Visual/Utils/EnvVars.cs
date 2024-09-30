d953272c46c3fa916a56b1f437f12a60095a2ae0
Logan Graham <logan.graham@saucelabs.com>
2024-09-30 14:01:04 -0400

    disable nullish coalescing to empty strings for environment variables

--- visual-dotnet/SauceLabs.Visual/Utils/EnvVars.cs
+++ visual-dotnet/SauceLabs.Visual/Utils/EnvVars.cs
@@ -4,17 +4,17 @@ namespace SauceLabs.Visual.Utils
 {
     internal class EnvVars
     {
-        internal static string Project => Environment.GetEnvironmentVariable("SAUCE_VISUAL_PROJECT") ?? "";
-        internal static string Branch => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BRANCH") ?? "";
+        internal static string? Project => Environment.GetEnvironmentVariable("SAUCE_VISUAL_PROJECT");
+        internal static string? Branch => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BRANCH");
 
-        internal static string DefaultBranch =>
-            Environment.GetEnvironmentVariable("SAUCE_VISUAL_DEFAULT_BRANCH") ?? "";
+        internal static string? DefaultBranch =>
+            Environment.GetEnvironmentVariable("SAUCE_VISUAL_DEFAULT_BRANCH");
 
         internal static string BuildName => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BUILD_NAME") ?? "";
-        internal static string CustomId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_CUSTOM_ID") ?? "";
-        internal static string BuildId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BUILD_ID") ?? "";
-        internal static string Username => Environment.GetEnvironmentVariable("SAUCE_USERNAME") ?? "";
-        internal static string AccessKey => Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY") ?? "";
-        internal static string Region => Environment.GetEnvironmentVariable("SAUCE_REGION") ?? "";
+        internal static string? CustomId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_CUSTOM_ID");
+        internal static string? BuildId => Environment.GetEnvironmentVariable("SAUCE_VISUAL_BUILD_ID");
+        internal static string? Username => Environment.GetEnvironmentVariable("SAUCE_USERNAME");
+        internal static string? AccessKey => Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY");
+        internal static string? Region => Environment.GetEnvironmentVariable("SAUCE_REGION");
     }
 }
