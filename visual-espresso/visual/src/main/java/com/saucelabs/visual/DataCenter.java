5d4f186b182efa4e2a50b8e701306cda094c263f
Kerem <github@beygo.info>
2024-11-19 15:07:12 +0100

    add staging DC

--- visual-espresso/visual/src/main/java/com/saucelabs/visual/DataCenter.java
+++ visual-espresso/visual/src/main/java/com/saucelabs/visual/DataCenter.java
@@ -5,7 +5,8 @@ import com.saucelabs.visual.exception.VisualApiException;
 public enum DataCenter {
     US_WEST_1("https://api.us-west-1.saucelabs.com/v1/visual/graphql"),
     US_EAST_4("https://api.us-east-4.saucelabs.com/v1/visual/graphql"),
-    EU_CENTRAL_1("https://api.eu-central-1.saucelabs.com/v1/visual/graphql");
+    EU_CENTRAL_1("https://api.eu-central-1.saucelabs.com/v1/visual/graphql"),
+    STAGING("https://api.staging.saucelabs.net/v1/visual/graphql");
 
     public final String endpoint;
 
@@ -24,6 +25,8 @@ public enum DataCenter {
                 return EU_CENTRAL_1;
             case "us-east-4":
                 return US_EAST_4;
+            case "staging":
+                return STAGING;
             default:
                 throw new VisualApiException("Unknown region: " + sauceRegion);
         }
