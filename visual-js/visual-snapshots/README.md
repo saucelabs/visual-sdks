d37b7873ecc62095c35878fb9f39f0113f042140
Agnieszka Szczepanska <58401935+aga-szczepanska@users.noreply.github.com>
2025-02-20 08:33:06 +0100

    [INT-16] Initial visual-snapshots package (#188)
    Generate visual-snapshots CLI package with pdf command which for now does nothing

---
+++ visual-js/visual-snapshots/README.md
@@ -0,0 +1,29 @@
+# Sauce Labs Visual Snapshot CLI
+
+This package provides a CLI tool to create Visual snapshots of a provided PDF document.
+
+## Installation
+
+```sh
+npm install --save @saucelabs/visual-snapshots
+```
+
+## Development
+
+Build:
+
+```sh
+npm run build
+```
+
+Execute:
+
+```sh
+node lib/index.js [command] [params] 
+```
+
+Run tests:
+
+```sh
+npm run test
+```
---
+++ visual-js/visual-snapshots/jest.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  // [...]
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testPathIgnorePatterns: ['/lib/'],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
