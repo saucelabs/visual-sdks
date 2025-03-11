27f005c89b6f81a33d6bb636d2370f45c3ad83b7
Sebastian Alex <sebastian.alex@saucelabs.com>
2025-03-11 12:33:01 +0100

    visual-js: fix visual-storybook lint errors

--- visual-js/visual-storybook/.eslintrc.cjs
+++ visual-js/visual-storybook/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.lint.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
