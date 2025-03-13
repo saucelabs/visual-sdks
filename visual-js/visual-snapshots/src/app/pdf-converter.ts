c1880b10eeffbde1bb1e3cec2c5793474256f8dd
Sebastian Alex <sebastian.alex@saucelabs.com>
2025-03-13 10:39:30 +0100

    [INT-1] visual-snapshots: add ESM support for Jest

--- visual-js/visual-snapshots/src/app/pdf-converter.ts
+++ visual-js/visual-snapshots/src/app/pdf-converter.ts
@@ -2,12 +2,12 @@ import { pdf } from "pdf-to-img";
 import { PdfFile } from "./pdf-file.js";
 
 export class PdfConverter {
+  constructor(private readonly _pdf: typeof pdf = pdf) {}
+
   public async *convertPagesToImages(
     pdfFilePath: string
   ): AsyncGenerator<Buffer> {
-    for await (const pdfPageImage of await pdf(pdfFilePath, { scale: 1 })) {
-      yield pdfPageImage;
-    }
+    return await this._pdf(pdfFilePath, { scale: 1 });
   }
 
   public createPdfFile(pdfFilePath: string): PdfFile {
--- visual-js/visual-snapshots/test/app/pdf-converter.spec.ts
+++ visual-js/visual-snapshots/test/app/pdf-converter.spec.ts
@@ -1,35 +1,28 @@
 import { PdfConverter } from "../../src/app/pdf-converter.js";
 
-jest.mock("pdf-to-img", () => {
-  async function* asyncIterable() {
-    for (let i = 0; i < 2; i++) {
-      yield `fake-image-buffer-${i}`;
-    }
-  }
-
-  return {
-    pdf: asyncIterable,
-  };
-});
-
 describe("PdfConverter", () => {
   test("convertPagesToImages", async () => {
-    const pdfFilePath = "./fake-pdf-file-path.pdf";
-    const pdfConverter = new PdfConverter();
-    const pdfPageImagesGenerator = await pdfConverter.convertPagesToImages(
-      pdfFilePath
-    );
+    const pdf = jest.fn();
 
-    for (let i = 0; i < 2; ++i) {
-      const pdfPageImage = await pdfPageImagesGenerator.next();
-      expect(pdfPageImage).toEqual({
-        done: false,
-        value: `fake-image-buffer-${i}`,
-      });
+    const pdfFilePath = "./fake-pdf-file-path.pdf";
+    const pdfConverter = new PdfConverter(pdf);
+    for await (const _ of pdfConverter.convertPagesToImages(pdfFilePath)) {
     }
-    expect(await pdfPageImagesGenerator.next()).toEqual({
-      done: true,
-      value: undefined,
-    });
+
+    expect(pdf).toHaveBeenCalledWith(pdfFilePath, { scale: 1 });
+  });
+
+  test("createPdfFile", async () => {
+    const pdf = jest.fn();
+    const pdfConverter = new PdfConverter(pdf);
+
+    const pdfFilePath = "./fake-pdf-file-path.pdf";
+    const pdfFile = pdfConverter.createPdfFile(pdfFilePath);
+
+    for await (const _ of pdfFile.convertPagesToImages()) {
+    }
+
+    expect(pdfFile.path).toBe(pdfFilePath);
+    expect(pdf).toHaveBeenCalledWith(pdfFilePath, { scale: 1 });
   });
 });
