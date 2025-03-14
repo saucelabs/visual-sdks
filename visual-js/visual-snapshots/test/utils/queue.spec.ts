4ba9a6383ac37e4619512a6dff3696ec1330c1a1
Sebastian Alex <sebastian.alex@saucelabs.com>
2025-03-14 15:49:37 +0100

    [INT-78] visual-snapshots: process pages before adding more files

--- visual-js/visual-snapshots/test/utils/queue.spec.ts
+++ visual-js/visual-snapshots/test/utils/queue.spec.ts
@@ -1,5 +1,20 @@
 import Queue from "queue";
-import { waitForEmptyQueue } from "../../src/utils/queue.js";
+import {
+  enqueueFromIterator,
+  waitForEmptyQueue,
+} from "../../src/utils/queue.js";
+
+function immediateWorker<T>(fn: () => T) {
+  return new Promise<T>((resolve, reject) => {
+    setImmediate(() => {
+      try {
+        resolve(fn());
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+}
 
 describe("waitForEmptyQueue", () => {
   it("should wait for all elements in queue to finish", async () => {
@@ -12,14 +27,11 @@ describe("waitForEmptyQueue", () => {
     let result = 0;
     const workers = 5;
     for (let i = 0; i < workers; i++) {
-      queue.push(() => {
-        return new Promise<void>((resolve) => {
-          setImmediate(() => {
-            result += 1;
-            resolve();
-          });
-        });
-      });
+      queue.push(() =>
+        immediateWorker(() => {
+          result += 1;
+        })
+      );
     }
 
     await waiter;
@@ -37,20 +49,219 @@ describe("waitForEmptyQueue", () => {
 
     const workers = 5;
     for (let i = 0; i < workers; i++) {
-      queue.push(
-        () =>
-          new Promise<void>((resolve, reject) => {
-            setImmediate(() => {
-              if (i === 3) {
-                reject(error);
-              } else {
-                resolve();
-              }
-            });
-          })
+      queue.push(() =>
+        immediateWorker(() => {
+          if (i === 3) {
+            throw new Error("test");
+          }
+        })
       );
     }
 
     expect(waiter).rejects.toThrow(error);
+  });
+});
+
+describe("enqueueFromIterator", () => {
+  it("should enqueue and complete all elements with concurrency greater than element count", async () => {
+    const elements = [...new Array(20)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 30,
+    });
+
+    const actual: number[] = [];
+    await enqueueFromIterator(
+      queue,
+      elements.values(),
+      (n) => () => immediateWorker(() => actual.push(n))
+    );
+
+    await waitForEmptyQueue(queue);
+    expect(actual.sort((a, b) => a - b)).toEqual(elements);
+  });
+
+  it("should enqueue and complete all elements with concurrency less than element count", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 10,
+    });
+
+    const actual: number[] = [];
+    await enqueueFromIterator(
+      queue,
+      elements.values(),
+      (n) => () => immediateWorker(() => actual.push(n))
+    );
+
+    await waitForEmptyQueue(queue);
+    expect(actual.sort((a, b) => a - b)).toEqual(elements);
+  });
+
+  it("should reject if first worker creator throws", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 1,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(queue, elements.values(), () => {
+      throw new Error("test");
+    });
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if n-th worker creator throws where n < concurrency", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 30,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(queue, elements.values(), (n) => {
+      if (n === 1) {
+        throw new Error("test");
+      }
+
+      return () =>
+        immediateWorker(() => {
+          // do nothing
+        });
+    });
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if n-th worker creator throws where n > concurrency", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+    const queue = new Queue({
+      concurrency: 1,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(queue, elements.values(), (n) => {
+      if (n === 90) {
+        throw new Error("test");
+      }
+
+      return () =>
+        immediateWorker(() => {
+          // do nothing
+        });
+    });
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if last worker creator throws", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 1,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(queue, elements.values(), (n) => {
+      if (n === elements.length - 1) {
+        throw new Error("test");
+      }
+
+      return () =>
+        immediateWorker(() => {
+          // do nothing
+        });
+    });
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if first worker throws", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 1,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(
+      queue,
+      elements.values(),
+      () => () =>
+        immediateWorker(() => {
+          throw new Error("test");
+        })
+    );
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if n-th worker throws where n < concurrency", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 30,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(
+      queue,
+      elements.values(),
+      (n) => () =>
+        immediateWorker(() => {
+          if (n === 1) {
+            throw new Error("test");
+          }
+        })
+    );
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if n-th worker throws where n > concurrency", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 1,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(
+      queue,
+      elements.values(),
+      (n) => () =>
+        immediateWorker(() => {
+          if (n === 90) {
+            throw new Error("test");
+          }
+        })
+    );
+
+    await expect(promise).rejects.toThrow(error);
+  });
+
+  it("should reject if last worker throws", async () => {
+    const elements = [...new Array(100)].map((_, i) => i);
+
+    const queue = new Queue({
+      concurrency: 1,
+    });
+
+    const error = new Error("test");
+    const promise = enqueueFromIterator(
+      queue,
+      elements.values(),
+      (n) => () =>
+        immediateWorker(() => {
+          if (n === elements.length - 1) {
+            throw new Error("test");
+          }
+        })
+    );
+
+    await expect(promise).rejects.toThrow(error);
   });
 });
