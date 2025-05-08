a9d826c2e5dce3ac75808f211f051c68422acb1f
Sebastian Alex <sebastian.alex@saucelabs.com>
2025-05-08 15:19:01 +0200

    [INT-251] visual-java: change x,y to final in WindowScroll

--- visual-java/src/main/java/com/saucelabs/visual/model/WindowScroll.java
+++ visual-java/src/main/java/com/saucelabs/visual/model/WindowScroll.java
@@ -1,8 +1,8 @@
 package com.saucelabs.visual.model;
 
 public class WindowScroll {
-  public int x;
-  public int y;
+  public final int x;
+  public final int y;
 
   public WindowScroll(int x, int y) {
     this.x = x;
