a6ae059be4f5f1d4dacc1908687a8efafb886bb0
Logan Graham <logan.graham@saucelabs.com>
2025-12-22 13:11:36 -0500

    lint

--- visual-java/src/main/java/com/saucelabs/visual/graphql/CreateSnapshotMutation.java
+++ visual-java/src/main/java/com/saucelabs/visual/graphql/CreateSnapshotMutation.java
@@ -3,7 +3,6 @@ package com.saucelabs.visual.graphql;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.saucelabs.visual.graphql.type.SnapshotIn;
-
 import java.util.Collections;
 import java.util.Map;
 
