09b57a8314e3a69b24412938096d3de8ed9f559a
Sebastian Alex <sebastian.alex@saucelabs.com>
2026-01-21 09:37:58 +0100

    [BIQ-763] add code generation to simplify BaselineOverride fields and properties

--- visual-dotnet/SauceLabs.Visual/BaselineOverride.cs
+++ visual-dotnet/SauceLabs.Visual/BaselineOverride.cs
@@ -1,3 +1,4 @@
+using SauceLabs.Visual.CodeGenerators;
 using SauceLabs.Visual.GraphQL;
 using SauceLabs.Visual.Models;
 using SauceLabs.Visual.Utils;
@@ -9,145 +10,24 @@ namespace SauceLabs.Visual
     /// when finding a baseline. Enables cross browser / OS snapshot comparison by matching the
     /// values set on the device you'd like to compare against.
     /// </summary>
-    public class BaselineOverride
+    public partial class BaselineOverride
     {
+        [ExplicitValueSet]
         private Browser? _browser;
-        private bool _browserSet = false;
-        public Browser? Browser
-        {
-            get => _browser;
-            set
-            {
-                _browser = value;
-                _browserSet = true;
-            }
-        }
-        public void UnsetBrowser()
-        {
-            _browser = null;
-            _browserSet = false;
-        }
-
+        [ExplicitValueSet]
         private string? _browserVersion;
-        private bool _browserVersionSet = false;
-        public string? BrowserVersion
-        {
-            get => _browserVersion;
-            set
-            {
-                _browserVersion = value;
-                _browserVersionSet = true;
-            }
-        }
-        public void UnsetBrowserVersion()
-        {
-            _browserVersion = null;
-            _browserVersionSet = false;
-        }
-
+        [ExplicitValueSet]
         private string? _device;
-        private bool _deviceSet = false;
-        public string? Device
-        {
-            get => _device;
-            set
-            {
-                _device = value;
-                _deviceSet = true;
-            }
-        }
-        public void UnsetDevice()
-        {
-            _device = null;
-            _deviceSet = false;
-        }
-
+        [ExplicitValueSet]
         private string? _name;
-        private bool _nameSet = false;
-        public string? Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                _nameSet = true;
-            }
-        }
-
-        public void UnsetName()
-        {
-            _name = null;
-            _nameSet = false;
-        }
+        [ExplicitValueSet]
         private OperatingSystem? _operatingSystem;
-        private bool _operatingSystemSet = false;
-        public OperatingSystem? OperatingSystem
-        {
-            get => _operatingSystem;
-            set
-            {
-                _operatingSystem = value;
-                _operatingSystemSet = true;
-            }
-        }
-        public void UnsetOperatingSystem()
-        {
-            _operatingSystem = null;
-            _operatingSystemSet = false;
-        }
-
+        [ExplicitValueSet]
         private string? _operatingSystemVersion;
-        private bool _operatingSystemVersionSet = false;
-        public string? OperatingSystemVersion
-        {
-            get => _operatingSystemVersion;
-            set
-            {
-                _operatingSystemVersion = value;
-                _operatingSystemVersionSet = true;
-            }
-        }
-
-        public void UnsetOperatingSystemVersion()
-        {
-            _operatingSystemVersion = null;
-            _operatingSystemVersionSet = false;
-        }
-
+        [ExplicitValueSet]
         private string? _suiteName;
-        private bool _suiteNameSet = false;
-        public string? SuiteName
-        {
-            get => _suiteName;
-            set
-            {
-                _suiteName = value;
-                _suiteNameSet = true;
-            }
-        }
-
-        public void UnsetSuiteName()
-        {
-            _suiteName = null;
-            _suiteNameSet = false;
-        }
-
+        [ExplicitValueSet]
         private string? _testName;
-        private bool _testNameSet = false;
-        public string? TestName
-        {
-            get => _testName;
-            set
-            {
-                _testName = value;
-                _testNameSet = true;
-            }
-        }
-        public void UnsetTestName()
-        {
-            _testName = null;
-            _testNameSet = false;
-        }
 
         internal BaselineOverrideIn ToBaselineOverrideIn()
         {
