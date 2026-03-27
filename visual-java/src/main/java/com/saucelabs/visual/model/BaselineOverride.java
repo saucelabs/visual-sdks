13b67fecd681eb6c96727891dd7a06bcd79372ab
Logan Graham <logan.graham@saucelabs.com>
2026-03-27 14:57:12 -0400

    wip

--- visual-java/src/main/java/com/saucelabs/visual/model/BaselineOverride.java
+++ visual-java/src/main/java/com/saucelabs/visual/model/BaselineOverride.java
@@ -16,64 +16,64 @@ public class BaselineOverride {
     return browser;
   }
 
-  public void setBrowser(Optional<Browser> browser) {
-    this.browser = browser;
+  public void setBrowser(Browser browser) {
+    this.browser = Optional.ofNullable(browser);
   }
 
   public Optional<OperatingSystem> getOperatingSystem() {
     return operatingSystem;
   }
 
-  public void setOperatingSystem(Optional<OperatingSystem> operatingSystem) {
-    this.operatingSystem = operatingSystem;
+  public void setOperatingSystem(OperatingSystem operatingSystem) {
+    this.operatingSystem = Optional.ofNullable(operatingSystem);
   }
 
   public Optional<String> getBrowserVersion() {
     return browserVersion;
   }
 
-  public void setBrowserVersion(Optional<String> browserVersion) {
-    this.browserVersion = browserVersion;
+  public void setBrowserVersion(String browserVersion) {
+    this.browserVersion = Optional.ofNullable(browserVersion);
   }
 
   public Optional<String> getDevice() {
     return device;
   }
 
-  public void setDevice(Optional<String> device) {
-    this.device = device;
+  public void setDevice(String device) {
+    this.device = Optional.ofNullable(device);
   }
 
   public Optional<String> getName() {
     return name;
   }
 
-  public void setName(Optional<String> name) {
-    this.name = name;
+  public void setName(String name) {
+    this.name = Optional.ofNullable(name);
   }
 
   public Optional<String> getOperatingSystemVersion() {
     return operatingSystemVersion;
   }
 
-  public void setOperatingSystemVersion(Optional<String> operatingSystemVersion) {
-    this.operatingSystemVersion = operatingSystemVersion;
+  public void setOperatingSystemVersion(String operatingSystemVersion) {
+    this.operatingSystemVersion = Optional.ofNullable(operatingSystemVersion);
   }
 
   public Optional<String> getSuiteName() {
     return suiteName;
   }
 
-  public void setSuiteName(Optional<String> suiteName) {
-    this.suiteName = suiteName;
+  public void setSuiteName(String suiteName) {
+    this.suiteName = Optional.ofNullable(suiteName);
   }
 
   public Optional<String> getTestName() {
     return testName;
   }
 
-  public void setTestName(Optional<String> testName) {
-    this.testName = testName;
+  public void setTestName(String testName) {
+    this.testName = Optional.ofNullable(testName);
   }
 
 
@@ -87,70 +87,86 @@ public class BaselineOverride {
     private Optional<String> suiteName;
     private Optional<String> testName;
 
-    public Builder withBrowser(Optional<Browser> browser) {
-      this.browser = browser;
+    public Builder withBrowser(Browser browser) {
+      this.browser = Optional.ofNullable(browser);
       return this;
     }
 
-    public Builder withOperatingSystem(Optional<OperatingSystem> operatingSystem) {
-      this.operatingSystem = operatingSystem;
+    public Builder withOperatingSystem(OperatingSystem operatingSystem) {
+      this.operatingSystem = Optional.ofNullable(operatingSystem);
       return this;
     }
 
-    public Builder withBrowserVersion(Optional<String> browserVersion) {
-      this.browserVersion = browserVersion;
+    public Builder withBrowserVersion(String browserVersion) {
+      this.browserVersion = Optional.ofNullable(browserVersion);
       return this;
     }
 
-    public Builder withDevice(Optional<String> device) {
-      this.device = device;
+    public Builder withDevice(String device) {
+      this.device = Optional.ofNullable(device);
       return this;
     }
 
-    public Builder withName(Optional<String> name) {
-      this.name = name;
+    public Builder withName(String name) {
+      this.name = Optional.ofNullable(name);
       return this;
     }
 
-    public Builder withOperatingSystemVersion(Optional<String> operatingSystemVersion) {
-      this.operatingSystemVersion = operatingSystemVersion;
+    public Builder withOperatingSystemVersion(String operatingSystemVersion) {
+      this.operatingSystemVersion = Optional.ofNullable(operatingSystemVersion);
       return this;
     }
 
-    public Builder withSuiteName(Optional<String> suiteName) {
-      this.suiteName = suiteName;
+    public Builder withSuiteName(String suiteName) {
+      this.suiteName = Optional.ofNullable(suiteName);
       return this;
     }
 
-    public Builder withTestName(Optional<String> testName) {
-      this.testName = testName;
+    public Builder withTestName(String testName) {
+      this.testName = Optional.ofNullable(testName);
       return this;
     }
 
     public BaselineOverride build() {
       BaselineOverride b = new BaselineOverride();
-      b.setBrowser(this.browser);
-      b.setOperatingSystem(this.operatingSystem);
-      b.setBrowserVersion(this.browserVersion);
-      b.setDevice(this.device);
-      b.setName(this.name);
-      b.setOperatingSystemVersion(this.operatingSystemVersion);
-      b.setSuiteName(this.suiteName);
-      b.setTestName(this.testName);
+      b.setBrowser(this.browser != null ? this.browser.get() : null);
+      b.setOperatingSystem(this.operatingSystem != null ? this.operatingSystem.get() : null);
+      b.setBrowserVersion(this.browserVersion != null ? this.browserVersion.get() : null);
+      b.setDevice(this.device != null ? this.device.get() : null);
+      b.setName(this.name != null ? this.name.get() : null);
+      b.setOperatingSystemVersion(this.operatingSystemVersion != null ? this.operatingSystemVersion.get() : null);
+      b.setSuiteName(this.suiteName != null ? this.suiteName.get() : null);
+      b.setTestName(this.testName != null ? this.testName.get() : null);
       return b;
     }
   }
 
   public BaselineOverrideInput asGraphQLType() {
     BaselineOverrideInput input = new BaselineOverrideInput();
-    input.setBrowser(this.browser != null ? this.browser.map(Browser::asGraphQLType).orElse(null) : null);
-    input.setOperatingSystem(this.operatingSystem != null ? this.operatingSystem.map(OperatingSystem::asGraphQLType).orElse(null) : null);
-    input.setBrowserVersion(this.browserVersion != null ? this.browserVersion.orElse(null): null);
-    input.setDevice(this.device != null ? this.device.orElse(null): null);
-    input.setName(this.name != null ? this.name.orElse(null): null);
-    input.setOperatingSystemVersion(this.operatingSystemVersion != null ? this.operatingSystemVersion.orElse(null): null);
-    input.setSuiteName(this.suiteName != null ? this.suiteName.orElse(null): null);
-    input.setTestName(this.testName != null ? this.testName.orElse(null): null);
+    if (this.browser != null) {
+      input.setBrowser(this.browser.map(Browser::asGraphQLType));
+    }
+    if (this.operatingSystem != null) {
+      input.setOperatingSystem(this.operatingSystem.map(OperatingSystem::asGraphQLType));
+    }
+    if (this.browserVersion != null) {
+      input.setBrowserVersion(this.browserVersion);
+    }
+    if (this.device != null) {
+      input.setDevice(this.device);
+    }
+    if (this.name != null) {
+      input.setName(this.name);
+    }
+    if (this.operatingSystemVersion != null) {
+      input.setOperatingSystemVersion(this.operatingSystemVersion);
+    }
+    if (this.suiteName != null) {
+      input.setSuiteName(this.suiteName);
+    }
+    if (this.testName != null) {
+      input.setTestName(this.testName);
+    }
     return input;
   }
 }
--- visual-java/src/main/java/com/saucelabs/visual/model/BaselineOverrideInput.java
+++ visual-java/src/main/java/com/saucelabs/visual/model/BaselineOverrideInput.java
@@ -41,4 +41,172 @@ public class BaselineOverrideInput extends BaselineOverrideIn {
   public Optional<Browser> browser(){
     return this.browser;
   }
+
+  @JsonProperty("operatingSystem")
+  @GraphQLScalar( fieldName = "operatingSystem", graphQLTypeSimpleName = "OperatingSystem", javaClass = com.saucelabs.visual.graphql.type.OperatingSystem.class, listDepth = 0)
+  private Optional<OperatingSystem> operatingSystem;
+
+  @Override
+  public void setOperatingSystem(OperatingSystem operatingSystem) {
+    this.operatingSystem = Optional.ofNullable(operatingSystem);
+  }
+
+  @Override
+  @JsonIgnore
+  public OperatingSystem getOperatingSystem() {
+    return this.operatingSystem.orElse(null);
+  }
+
+  public void setOperatingSystem(Optional<OperatingSystem> operatingSystem){
+    this.operatingSystem = operatingSystem;
+  }
+
+  @JsonProperty("operatingSystem")
+  public Optional<OperatingSystem> operatingSystem(){
+    return this.operatingSystem;
+  }
+
+  @JsonProperty("browserVersion")
+  @GraphQLScalar( fieldName = "browserVersion", graphQLTypeSimpleName = "String", javaClass = java.lang.String.class, listDepth = 0)
+  private Optional<String> browserVersion;
+
+  @Override
+  public void setBrowserVersion(String browserVersion) {
+    this.browserVersion = Optional.ofNullable(browserVersion);
+  }
+
+  @Override
+  @JsonIgnore
+  public String getBrowserVersion() {
+    return this.browserVersion.orElse(null);
+  }
+
+  public void setBrowserVersion(Optional<String> browserVersion){
+    this.browserVersion = browserVersion;
+  }
+
+  @JsonProperty("browserVersion")
+  public Optional<String> browserVersion(){
+    return this.browserVersion;
+  }
+
+  @JsonProperty("device")
+  @GraphQLScalar( fieldName = "device", graphQLTypeSimpleName = "String", javaClass = java.lang.String.class, listDepth = 0)
+  private Optional<String> device;
+
+  @Override
+  public void setDevice(String device) {
+    this.device = Optional.ofNullable(device);
+  }
+
+  @Override
+  @JsonIgnore
+  public String getDevice() {
+    return this.device.orElse(null);
+  }
+
+  public void setDevice(Optional<String> device){
+    this.device = device;
+  }
+
+  @JsonProperty("device")
+  public Optional<String> device(){
+    return this.device;
+  }
+
+  @JsonProperty("name")
+  @GraphQLScalar( fieldName = "name", graphQLTypeSimpleName = "String", javaClass = java.lang.String.class, listDepth = 0)
+  private Optional<String> name;
+
+  @Override
+  public void setName(String name) {
+    this.name = Optional.ofNullable(name);
+  }
+
+  @Override
+  @JsonIgnore
+  public String getName() {
+    return this.name.orElse(null);
+  }
+
+  public void setName(Optional<String> name){
+    this.name = name;
+  }
+
+  @JsonProperty("name")
+  public Optional<String> name(){
+    return this.name;
+  }
+
+  @JsonProperty("operatingSystemVersion")
+  @GraphQLScalar( fieldName = "operatingSystemVersion", graphQLTypeSimpleName = "String", javaClass = java.lang.String.class, listDepth = 0)
+  private Optional<String> operatingSystemVersion;
+
+  @Override
+  public void setOperatingSystemVersion(String operatingSystemVersion) {
+    this.operatingSystemVersion = Optional.ofNullable(operatingSystemVersion);
+  }
+
+  @Override
+  @JsonIgnore
+  public String getOperatingSystemVersion() {
+    return this.operatingSystemVersion.orElse(null);
+  }
+
+  public void setOperatingSystemVersion(Optional<String> operatingSystemVersion){
+    this.operatingSystemVersion = operatingSystemVersion;
+  }
+
+  @JsonProperty("operatingSystemVersion")
+  public Optional<String> operatingSystemVersion(){
+    return this.operatingSystemVersion;
+  }
+
+  @JsonProperty("suiteName")
+  @GraphQLScalar( fieldName = "suiteName", graphQLTypeSimpleName = "String", javaClass = java.lang.String.class, listDepth = 0)
+  private Optional<String> suiteName;
+
+  @Override
+  public void setSuiteName(String suiteName) {
+    this.suiteName = Optional.ofNullable(suiteName);
+  }
+
+  @Override
+  @JsonIgnore
+  public String getSuiteName() {
+    return this.suiteName.orElse(null);
+  }
+
+  public void setSuiteName(Optional<String> suiteName){
+    this.suiteName = suiteName;
+  }
+
+  @JsonProperty("suiteName")
+  public Optional<String> suiteName(){
+    return this.suiteName;
+  }
+
+  @JsonProperty("testName")
+  @GraphQLScalar( fieldName = "testName", graphQLTypeSimpleName = "String", javaClass = java.lang.String.class, listDepth = 0)
+  private Optional<String> testName;
+
+  @Override
+  public void setTestName(String testName) {
+    this.testName = Optional.ofNullable(testName);
+  }
+
+  @Override
+  @JsonIgnore
+  public String getTestName() {
+    return this.testName.orElse(null);
+  }
+
+  public void setTestName(Optional<String> testName){
+    this.testName = testName;
+  }
+
+  @JsonProperty("testName")
+  public Optional<String> testName(){
+    return this.testName;
+  }
 }
